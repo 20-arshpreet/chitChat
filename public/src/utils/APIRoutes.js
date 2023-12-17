c5030483c5ebae558d836fecb54cb676bada8b2d
20-arshpreet <77087214+20-arshpreet@users.noreply.github.com>
2023-12-17 16:50:48 +0530

    first commit

---
+++ public/src/utils/APIRoutes.js
@@ -0,0 +1,7 @@
+export const host= "http://localhost:5000";
+export const RegisterRoute=`${host}/api/auth/register`;
+export const LoginRoute=`${host}/api/auth/login`;
+export const AvatarRoute=`${host}/api/auth/avatar`;
+export const AllUsersRoute=`${host}/api/auth/allusers`;
+export const SendMessageRoute=`${host}/api/messages/addmsg`;
+export const GetAllMessageRoute=`${host}/api/messages/getmsg`;
